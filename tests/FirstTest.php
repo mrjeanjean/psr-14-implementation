9ea6688fe2fa113e2381f62b4f5c81690cf64098
Gaëtan <gaetan@moveo-webdesign.com>
2021-04-11 22:11:20 +0200

    Initialization and CI configuration

---
+++ tests/FirstTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testShouldPass(): void
+    {
+        $this->assertTrue(true);
+
+    }
+}
